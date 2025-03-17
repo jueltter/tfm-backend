c789db1b7cf652845fcf313d1af96abe0cc73752
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-17 03:57:37 -0500

    initial commit

---
+++ src/test/java/org/example/tfmbackend/TfmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tfmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
